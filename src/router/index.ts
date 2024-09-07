14243b23ed450a3454c6e0015337ad4478b6bf32
Roman <romazelenskiy2000@gmail.com>
2024-09-07 23:16:51 +0300

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
-// import { getCurrentUser } from "vuefire";
+import { getCurrentUser } from "vuefire";
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
