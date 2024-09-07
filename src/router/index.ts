ce6661c56a56ad5d87569dc0384ed623992a4100
Roman <romazelenskiy2000@gmail.com>
2024-09-08 00:03:55 +0300

    test

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,8 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 import { getCurrentUser } from "vuefire";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.VITE_BASE_URL + "/"),
+  history: createWebHashHistory(import.meta.env.VITE_BASE_URL),
   routes: [
     {
       name: "Auth",
