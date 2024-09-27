581399f8468152908c22903cb79b44158da5d445
roman-zelenskiy <73440148+roman-zelenskiy@users.noreply.github.com>
2024-09-27 09:22:18 +0000

    Deploying to gh-pages from @ roman-zelenskiy/admin_vue@d0b4fbd10082731974cfd62c31401823c22f4953 🚀

---
+++ assets/auth-1sLwWnfl.js
@@ -0,0 +1,1 @@
+import{j as s,i as u,r as o,o as r,E as a,G as n}from"./index-CgwtBPp_.js";const l=s("auth",()=>{const e=u(),t=o(null);return r(async()=>{t.value=await a()}),{logout:()=>n(e),user:t}});export{l as u};
