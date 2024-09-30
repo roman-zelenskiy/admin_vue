f16c740b84133774f5aac905b74b5779d87231d4
roman-zelenskiy <73440148+roman-zelenskiy@users.noreply.github.com>
2024-09-30 16:35:52 +0000

    Deploying to gh-pages from @ trusted-network-partner/admin@827c9ea656c47224bfad6e64b96905ce214ea8e6 🚀

--- assets/auth-B58Jrx2G.js
+++
@@ -1,1 +0,0 @@
-import{j as s,i as u,r as o,o as r,E as a,G as n}from"./index-CjvAX0Hz.js";const l=s("auth",()=>{const e=u(),t=o(null);return r(async()=>{t.value=await a()}),{logout:()=>n(e),user:t}});export{l as u};
